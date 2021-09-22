b1bdc54850dac18e8dc4c5ae9e7f6946da265a7e
Romain Coquery <romaincoquery@gmail.com>
2021-09-22 10:25:23 +0200

    création fonction Scraping des informations d'un livre, fonction récupération des url des catégories du site, fonction pour appeler et analyser les données d'une page d'un site, fonction pour les étoiles des avis des livres

--- instructions.py
+++ instructions.py
@@ -1,28 +1,15 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 main_url = "https://books.toscrape.com/"
-url = "https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
+purl = "https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-product_page_url = url
-title = soup.find("div", class_="product_main").find("h1").string
-category = soup.find("ul", class_="breadcrumb").find_all("a")
-category = category[2].string
-product_description = soup.find("article", class_="product_page").find("p", recursive=False).string
-try:
-    product_description = product_description
-except AttributeError:
-    description = ""
-img = soup.find("div", class_="thumbnail").find("img")
-img_url = img['src'].replace('../../', main_url)
-product_info = soup.find("table", class_="table-striped").find_all("td")
-universal_product_code = product_info[0].string
-price_excluding_tax = product_info[2].string
-price_including_tax = product_info[3].string
-stock = product_info[5].string
-number_available = stock.replace("In stock (", "").replace(" available)", "")
+
+def get_parsed(url):
+    """Fonction pour appeler et analyser une page du site"""
+    response = requests.get(url)
+    soup = bs(response.content, 'html.parser')
+    return soup
 
 
 def product_rating(star):
@@ -44,6 +31,37 @@ def product_rating(star):
     return star
 
 
-review_rating = product_rating(str(soup.find("p", class_="star-rating")))
-print(product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax,
-      number_available, category, review_rating, img_url, product_description)
+def list_url_category():
+    """Récupération des url des catégories"""
+    for list_url in get_parsed(main_url).find('ul', class_="nav nav-list").find_all('a', href=True):
+        print(main_url + list_url['href'])
+
+
+def product_book(purl):
+    """Récupérer les données d un livre"""
+    soup = get_parsed(purl)
+    product_page_url = purl
+    title = soup.find("div", class_="product_main").find("h1").string
+    category = soup.find("ul", class_="breadcrumb").find_all("a")
+    category = category[2].string
+    product_description = soup.find("article", class_="product_page").find("p", recursive=False).string
+    try:
+        product_description = product_description
+    except AttributeError:
+        product_description = ''
+    img = soup.find("div", class_="thumbnail").find("img")
+    img_url = img['src'].replace('../../', main_url)
+    product_info = soup.find("table", class_="table-striped").find_all("td")
+    universal_product_code = product_info[0].string
+    price_excluding_tax = product_info[2].string
+    price_including_tax = product_info[3].string
+    stock = product_info[5].string
+    number_available = stock.replace("In stock (", "").replace(" available)", "")
+    review_rating = product_rating(str(soup.find("p", class_="star-rating")))
+    return product_page_url, title, category, img_url, universal_product_code,\
+        price_excluding_tax, price_including_tax, number_available, review_rating, \
+        product_description,
+
+
+print(product_book(purl))
+print(list_url_category())
