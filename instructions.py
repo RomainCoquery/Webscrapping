13385685aa1044c2730d38542ccb8ba5d2b437ab
Romain Coquery <romaincoquery@gmail.com>
2021-09-28 08:50:54 +0200

    Revert "création fonction Scraping des informations d'un livre, fonction récupération des url des catégories du site, fonction pour appeler et analyser les données d'une page d'un site, fonction pour les étoiles des avis des livres"
    This reverts commit 46d05983bde2f6642dc87511d6f8559300b5b26e.

--- instructions.py
+++ instructions.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-main_url = "http://books.toscrape.com/"
-book = "http://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
+main_url = "https://books.toscrape.com/"
+purl = "https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html"
 
 
 def get_parsed(url):
@@ -10,25 +10,6 @@ def get_parsed(url):
     response = requests.get(url)
     soup = bs(response.content, 'html.parser')
     return soup
-
-
-def list_url_category(url):
-    """Récupération des liens de chaque catégorie"""
-    category_url = []
-    for list_url in get_parsed(url).find('ul', class_="nav").find_all('a', href=True):
-        category_url.append([url + list_url['href'], list_url.text.strip()])
-    category_url.pop(0)
-    return category_url
-
-
-def list_page_category():
-    """Récupération des liens des pages dans les catégories"""
-    for category_url, category_name in list_url_category(main_url):
-        category_page = get_parsed(category_url)
-        for element in category_page.find_all('h3'):
-            print('http://books.toscrape.com/catalogue/' + element.a.get('href')
-                  .replace('../../../', ''))
-    return category_url
 
 
 def product_rating(star):
@@ -50,15 +31,20 @@ def product_rating(star):
     return star
 
 
-def product_book(book_url):
+def list_url_category():
+    """Récupération des url des catégories"""
+    for list_url in get_parsed(main_url).find('ul', class_="nav nav-list").find_all('a', href=True):
+        print(main_url + list_url['href'])
+
+
+def product_book(purl):
     """Récupérer les données d un livre"""
-    soup = get_parsed(book_url)
-    product_page_url = book_url
-    title = soup.find("div", class_="product_main").find("h1").text
+    soup = get_parsed(purl)
+    product_page_url = purl
+    title = soup.find("div", class_="product_main").find("h1").string
     category = soup.find("ul", class_="breadcrumb").find_all("a")
-    category = category[2].text
-    product_description = soup.find("article", class_="product_page").find("p", recursive=False)\
-        .text
+    category = category[2].string
+    product_description = soup.find("article", class_="product_page").find("p", recursive=False).string
     try:
         product_description = product_description
     except AttributeError:
@@ -66,26 +52,16 @@ def product_book(book_url):
     img = soup.find("div", class_="thumbnail").find("img")
     img_url = img['src'].replace('../../', main_url)
     product_info = soup.find("table", class_="table-striped").find_all("td")
-    universal_product_code = product_info[0].text
-    price_excluding_tax = product_info[2].text
-    price_including_tax = product_info[3].text
-    stock = product_info[5].text
+    universal_product_code = product_info[0].string
+    price_excluding_tax = product_info[2].string
+    price_including_tax = product_info[3].string
+    stock = product_info[5].string
     number_available = stock.replace("In stock (", "").replace(" available)", "")
     review_rating = product_rating(str(soup.find("p", class_="star-rating")))
-    return {
-        'product_page_url': product_page_url,
-        'title': title,
-        'category': category,
-        'img_url': img_url,
-        'universal_product_code': universal_product_code,
-        'price_excluding_tax': price_excluding_tax,
-        'price_including_tax': price_including_tax,
-        'number_available': number_available,
-        'review_rating': review_rating,
-        'product_description': product_description,
-    }
+    return product_page_url, title, category, img_url, universal_product_code,\
+        price_excluding_tax, price_including_tax, number_available, review_rating, \
+        product_description,
 
 
-print(product_book(book))
-print(list_url_category(main_url))
-print(list_page_category())
+print(product_book(purl))
+print(list_url_category())
