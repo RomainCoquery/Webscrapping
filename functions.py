00f7af17be4b6730e0db19fb0a28cc2b3b0356c1
Thierry Chappuis <tchappui@users.noreply.github.com>
2021-10-01 09:40:50 +0200

    Added comments to final.py

--- functions.py
+++ functions.py
@@ -21,7 +21,9 @@ def get_categories_urls(url):
     """Récupération des liens de chaque catégorie"""
     category_url = []
     for list_url in get_parsed(url).find('ul', class_="nav").find_all('a', href=True):
+        # -tc- utiliser urljoin() pour concaténer les urls, plutôt que l'opérateur +
         category_url.append([url + list_url['href'], list_url.text.strip()])
+    # -tc- pourquoi insérer la première url dans la liste si on ne la veut pas ?
     category_url.pop(0)
     return category_url
 
@@ -39,16 +41,20 @@ def get_category_pages(category_url):
 def get_pages_for_category(url):
     categories_url = []
     for list_url in get_parsed(url).find('ul', class_="nav").find_all('a', href=True):
+        # -tc- utiliser de préférence la fonction urllib.parse.urljoin() pour coller main_url et list_url['href']
         category_url = main_url + list_url['href']
         next_button = get_parsed(category_url).find('li', class_='next')
         while next_button:
+            # -tc- comme ci-dessus, utiliser urllib.parse.urljoin() pour coller les morceaux d'url. replace n'est ni nécessaire ni souhaité
             page = category_url.replace('index.html', '') + (next_button.find('a')['href'])
             next_button = get_parsed(page).find('li', class_='next')
             categories_url.append(page)
+            # -tc- le continue ici ne sert à rien, à enlever
             continue
     return categories_url
 
 
+# -tc- Il semblerait plus logique ici d'utiliser un dictionnaire. Plus efficace et... c'est fait pour cela
 def product_rating(star):
     """Fonction pour avoir le nombre d etoiles d avis"""
     if "Zero" in star:
@@ -75,20 +81,32 @@ def product_book(book_url):
     title = soup.find("div", class_="product_main").find("h1").text
     category = soup.find("ul", class_="breadcrumb").find_all("a")
     category = category[2].text
+    # -tc- éviter d'usage de \ à l'aide de parenthèses
+    # product_description = (
+    #     soup
+    #     .find("article", class_="product_page")
+    #     .find("p", recursive=False)
+    #     .text
+    # )
+    # -tc- attention, l'exception aura lieu ici si pas de description
     product_description = soup.find("article", class_="product_page").find("p", recursive=False)\
         .text
+    # -tc- ne sert à rien. Plutôt if product_description is not None:
     try:
         product_description = product_description
     except AttributeError:
         product_description = ''
     img = soup.find("div", class_="thumbnail").find("img")
+    # -tc- éviter replace, urllib.parse.urljoin() est faite pour cela
     img_url = img['src'].replace('../../', main_url)
     product_info = soup.find("table", class_="table-striped").find_all("td")
+    # -tc- Si une info manque ici, il y aura une KeyError
     universal_product_code = product_info[0].text
     price_excluding_tax = product_info[2].text
     price_including_tax = product_info[3].text
     stock = product_info[5].text
     number_available = stock.replace("In stock (", "").replace(" available)", "")
+    # -tc- comme dit plus haut, probablement plus adapté d'utiliser un dictionnaire ici
     review_rating = product_rating(str(soup.find("p", class_="star-rating")))
     return {
         'product_page_url': product_page_url,
@@ -108,3 +126,14 @@ def product_book(book_url):
 # print(get_category_pages(main_url))
 print(get_pages_for_category(main_url))
 # print(product_book(book))
+
+# -tc- dans l'idéal ici, on va ici éviter que la fonction get_pages_for_category(main_url) ne s'exécute lors d'un import.
+# if __name__ == "__main__":
+#     print(get_pages_for_category(main_url))
+# 
+# -tc- dans l'idéal, on a également l'habitude de créer une fonction appelée main() pour y mettre le code de démarrage du programme
+# def main():
+#     print(get_pages_for_category(main_url))
+#
+# if __name__ == "__main__":
+#     print(get_pages_for_category(main_url))
